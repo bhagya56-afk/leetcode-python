a57fe959ccc6bf401dca5a7c128f81a2cf8da79f
bhagya56-afk <sanemostly116@gmail.com>
2026-06-23 19:35:44 +0530

    Day 1 12th intersection-of-two-arrays

---
+++ day01_12_intersection_array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        num1=set(nums1)
+        num2=set(nums2)
+        lst=[]
+        for i in num1:
+            if i in num2:
+                
+                lst+=[i]
+        return lst
+        
