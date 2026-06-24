8006f90cfbeee4eda99179d01f927b247610684e
bhagya56-afk <sanemostly116@gmail.com>
2026-06-24 11:47:08 +0530

    Day 2 5th valid mountain array

---
+++ day02_5_valid_mountain.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def validMountainArray(self, arr):
+        n = len(arr)
+
+        if n < 3:
+            return False
+
+        peak = arr.index(max(arr))
+
+        if peak == 0 or peak == n - 1:
+            return False
+
+        for i in range(peak):
+            if arr[i] >= arr[i + 1]:
+                return False
+
+        for i in range(peak, n - 1):
+            if arr[i] <= arr[i + 1]:
+                return False
+
+        return True
